9b4e95e95b744fe6cc4cf4ca6ee998aabb7ddcd7
Jes Cok <xigua67damn@gmail.com>
2025-07-13 15:21:03 +0800

    test

--- tpl_test.go
+++ tpl_test.go
@@ -1,15 +1,36 @@
 package tpl_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/callthingsoff/tpl"
 )
 
-func TestRun(t *testing.T) {
+func TestFetch(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		case "/a/b/cpu":
+			w.Write([]byte(`{"a": {"b": {"c": [1,2,3]}}}`))
+		case "/x/y/memory":
+			w.Write([]byte(`{"a": {"b": {"c": [3]}}}`))
+		case "/storage":
+			w.Write([]byte(`{"storage": "/storages"}`))
+		case "/storages":
+			w.Write([]byte(`["/storage1", "/storage2"]`))
+		case "/storage1":
+			w.Write([]byte(`{"s":"v1"}`))
+		case "/storage2":
+			w.Write([]byte(`{"s":"v2"}`))
+		}
+	}))
+	defer server.Close()
+
 	b, err := os.ReadFile("tpl.yaml")
 	if err != nil {
 		t.Fatal(err)
@@ -19,8 +40,9 @@ func TestRun(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	val, err := tpl.NewFetcher(&tpl.Option{
-		IP:         "localhost",
+	got, err := tpl.NewFetcher(&tpl.Option{
+		HTTPS:      false,
+		IP:         server.Listener.Addr().String(),
 		User:       "x",
 		Password:   "y",
 		TimeoutSec: 3,
@@ -35,5 +57,38 @@ func TestRun(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("%v", val)
+	want := map[string]any{
+		"cpu": []int{
+			1,
+			2,
+			3,
+		},
+		"memory": []int{
+			3,
+		},
+		"ncpu":    3,
+		"nmemory": 1,
+		"storage": []string{
+			"/storage1",
+			"/storage2",
+		},
+		"storages": []string{
+			"v1",
+			"v2",
+		},
+		"sumcpu":    6,
+		"sumdivcpu": 2,
+		"summemory": 3,
+	}
+	x, err := json.Marshal(got)
+	if err != nil {
+		t.Fatal(err)
+	}
+	y, err := json.Marshal(want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(x, y) {
+		t.Fatalf("not equal:\nwant:\n%s\ngot:\n%s\n", x, y)
+	}
 }
